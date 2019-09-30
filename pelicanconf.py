f38dc0b4a6d31641200e4f8aede145ac54187ba2
Alexander H. Gose <alexgose@users.noreply.github.com>
2019-09-30 15:32:59 -0400

    added pelican-bootstrap3 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,9 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+THEME = 'theme'
+PLUGIN_PATHS = ['plugins/', ]
+PLUGINS = ['i18n_subsites', ]
+JINJA_ENVIRONMENT = {
+    'extensions': ['jinja2.ext.i18n'],
+}
